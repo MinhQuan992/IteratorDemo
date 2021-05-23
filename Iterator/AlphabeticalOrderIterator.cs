cc7fa453cc6e386fd73ae4310e65b1828d0805b8
Võ Trần Minh Quân <deesatoshi@gmail.com>
2021-05-23 23:15:18 +0700

    First commit

---
+++ Iterator/AlphabeticalOrderIterator.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iterator
+{
+    class AlphabeticalOrderIterator : Iterator
+    {
+        private WordsCollection collection;
+
+        // Stores the current traversal position. An iterator may have a lot of
+        // other fields for storing iteration state, especially when it is
+        // supposed to work with a particular kind of collection.
+        private int position = -1;
+
+        private bool reverse = false;
+
+        public AlphabeticalOrderIterator(WordsCollection collection, bool reverse = false)
+        {
+            this.collection = collection;
+            this.reverse = reverse;
+
+            if (reverse)
+            {
+                this.position = collection.getItems().Count;
+            }
+        }
+
+        public override int Key()
+        {
+            return this.position;
+        }
+
+        public override object Current()
+        {
+            return this.collection.getItems()[position];
+        }
+
+
+        public override bool MoveNext()
+        {
+            int updatedPosition = this.position + (this.reverse ? -1 : 1);
+
+            if (updatedPosition >= 0 && updatedPosition < this.collection.getItems().Count)
+            {
+                this.position = updatedPosition;
+                return true;
+            }
+            
+            return false;
+        }
+
+        public override void Reset()
+        {
+            this.position = this.reverse ? this.collection.getItems().Count - 1 : 0;
+        }
+    }
+}
---
+++ Iterator/WordsCollection.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iterator
+{
+    class WordsCollection : IteratorAggregate
+    {
+        List<string> collection = new List<string>();
+
+        bool direction = false;
+
+        public void ReverseDirection()
+        {
+            direction = !direction;
+        }
+
+        public List<string> getItems()
+        {
+            return collection;
+        }
+
+        public void AddItem(string item)
+        {
+            this.collection.Add(item);
+        }
+
+        public override object Current => throw new NotImplementedException();
+
+        public override IEnumerator GetEnumerator()
+        {
+            return new AlphabeticalOrderIterator(this, direction);
+        }
+
+        public override bool MoveNext()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Reset()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
